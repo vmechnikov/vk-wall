0356924d2b9ba31dac04491c2880e334fa9c29b2
Volodymyr <bitriani@gmail.com>
2020-01-20 12:31:52 +0200

    feature-basic-settings: set basic settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Layout from './layout';
+import rootReducer from './data/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk)),
+);
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Layout />
+	</Provider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/layout/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomePage from '../pages/HomePages';
+
+const Layout = () => (
+	<React.Fragment>
+		<HomePage />
+	</React.Fragment>
+);
+
+export default Layout;
